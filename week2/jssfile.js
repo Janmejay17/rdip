66738828485b059e7ea28bc05f6efa0f316e14ca
Janmejay17 <janmejaychauhan.nightwalker@gmail.com>
2020-07-14 16:44:13 +0530

    Task3

--- week2/jssfile.js
+++ week2/jssfile.js
@@ -1,3 +1,4 @@
+//task1
 function multiply(){
 a=Number(document.calculator.number1.value);
 b=Number(document.calculator.number2.value);
@@ -49,6 +50,7 @@ a=Number(document.calculator.number1.value);
 c=Math.sqrt(a);
 document.calculator.total.value=c;
 }
+//task2
 function allLetter()
   { 
   var uname = document.registration.username;
@@ -101,3 +103,76 @@ function allnumeric()
   return false;
   }
   }
+//task3
+
+//task 3
+//palindromes
+function TestFunction()
+{
+var InputStr = document.getElementById('tbox').value; 
+var resStr = checkingPalindrome(InputStr);
+alert('The input string "'+InputStr+'" is "'+resStr+'"'); 
+}
+function checkingPalindrome(InputStr)
+{
+var origString;
+InputStr = InputStr.toLowerCase();
+
+origString= InputStr;
+
+InputStr = InputStr.split(''); 
+InputStr = InputStr.reverse();
+InputStr = InputStr.join(''); 
+var revString = InputStr;
+if(origString == revString){
+return 'Palindrome word';
+}
+else
+{
+return 'not a Palindrome word';
+}
+}
+
+//anagrams
+
+
+
+
+function checkAnagram() {
+  var str1 = document.getElementById('word1').value;
+  var str2 = document.getElementById('word1').value;
+  if(str1 !== null && str2 !== null) {
+    if(str1.length !== str2.length) {
+      alert("Words are not anagrams.");
+      return false;
+    }
+    var hTable = {};
+    for(var i = 0; i < str1.length; i++) {
+      if(hTable.hasOwnProperty(str1[i])) {
+        hTable[str1[i]] = hTable[str1[i]] + 1;
+    } else {
+     hTable[str1[i]] = 1; 
+    }
+  }
+   
+  for(var i = 0; i < str2.length; i++) {
+    if(hTable.hasOwnProperty(str2[i])) {
+      hTable[str2[i]] = hTable[str2[i]] - 1;
+    } else {
+      alert("Words are not anagrams.");
+      return false;
+    }
+  }
+    
+  for(var i in hTable) {
+    if(hTable[i] !== 0) {
+      alert("Words are not anagrams.");
+      return false;
+    } else {
+       alert("Words are anagrams!");
+       break;
+    }
+  }
+}
+}
+//task4
